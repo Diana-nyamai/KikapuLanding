cd27b28cd5bcfe3777ff6971d94c9b6112eff3f5
Diana-nyamai <dnyamai.dn@gmail.com>
2023-10-14 18:15:25 +0300

    Completed home section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import {Navbar} from './components/Navbar/index'
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
