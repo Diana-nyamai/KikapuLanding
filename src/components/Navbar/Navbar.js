05a028480ab75ec104161e66a24b92fd2cf84aea
Diana-nyamai <dnyamai.dn@gmail.com>
2023-10-14 15:23:04 +0300

    Navbar completion

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
+import './navbar.css'
 
 export const Navbar = () => {
-  return (
-    <div>
-      <h2>i am navbar</h2>
-    </div>
-  )
-}
+  const navRef = useRef();
 
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsiveNav");
+  };
+
+  return (
+    <header>
+      <h2>
+        Kikapu<span>.</span>
+      </h2>
+      <nav ref={navRef}>
+        <Link to={"#"}>Home</Link>
+        <Link to={"#"}>Features</Link>
+        <Link to={"#"}>Team</Link>
+        <Link to={"#"}>Contact</Link>
+        <button className="loginButton">signup/login</button>
+        <button className="navBtn navCloseBtn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="navBtn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+};
