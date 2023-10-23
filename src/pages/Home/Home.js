489ac0cd224b12ccf54d70fa5eeedac529ed0266
Diana-nyamai <dnyamai.dn@gmail.com>
2023-10-23 14:38:07 +0300

    Updating hOME DESIGN

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom'
 import womanFruits from '../../assets/womanFruits.png'
+import playStore from '../../assets/icons/playStore.png'
+import appStore from '../../assets/icons/appStore.png'
 
 export const Home = () => {
   return (
@@ -8,8 +11,11 @@ export const Home = () => {
           <p>Revolutionizing Food Supply Chain for Africa's University <span style={{color: '#18D26E'}}>Students</span> </p>
         <p className="leftText">Kikapu provides food supply chain data-driven payment services that connects food producers (farmers), restaurants and university students (consumers)in Africa through a demand and supply simulation model.</p>
 
-        <button className="getStarted">Get Started</button>
-
+        <h3><span style={{color: '#18D26E'}}>DOWNLOAD</span> APP NOW</h3>
+        <div className="appLinks">
+          <Link to={'#'}><img src={playStore} alt='playStore'/></Link>
+          <Link to={'#'}><img src={appStore} alt='appStore'/></Link>
+        </div>
         </div>
         
       </div>
