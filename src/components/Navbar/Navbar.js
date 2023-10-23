e5bb4da5a49ca565828f79bf56b90aec4e11ee3f
Diana-nyamai <dnyamai.dn@gmail.com>
2023-10-23 14:26:34 +0300

    Changing navigation design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { BiDownload} from "react-icons/bi";
 import { useRef } from "react";
 
 export const Navbar = () => {
@@ -16,14 +17,19 @@ export const Navbar = () => {
       </h2>
       <nav ref={navRef}>
         <Link to={"#"}>Home</Link>
+        <Link to={"#"}>About</Link>
         <Link to={"#"}>Features</Link>
         <Link to={"#"}>Team</Link>
         <Link to={"#"}>Contact</Link>
-        <button className="loginButton">signup/login</button>
+        <button className="downloadButtonSmall">Download <BiDownload className="downloadIcon"/> </button>
         <button className="navBtn navCloseBtn" onClick={showNavbar}>
           <FaTimes />
         </button>
       </nav>
+      <div className="myDownloadButton">
+        <button className="downloadButton">Download <BiDownload className="downloadIcon"/> </button>
+      </div>
+      
       <button className="navBtn" onClick={showNavbar}>
         <FaBars />
       </button>
