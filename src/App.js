a8da0f98ca794468ba5c68fd6ad1bff56836d585
Diana-nyamai <dnyamai.dn@gmail.com>
2023-10-24 23:35:44 +0300

    Check APP section complete

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.scss';
 import { Footer } from './components/Footer/Footer';
 import {Navbar} from './components/Navbar/index'
 import { About } from './pages/About';
+import { CheckApp } from './pages/CheckApp';
 import { Features } from './pages/Features';
 import { Home } from './pages/Home/Home';
-import { Sucess } from './pages/Success/Sucess';
 import { Team } from './pages/Team/index';
 import { Share } from './pages/shareJourney/Share';
 
@@ -15,8 +15,8 @@ function App() {
       <Home/>
       <About/>
       <Features/>
+      <CheckApp/>
       <Share/>
-      <Sucess/>
       <Team/>
       <Footer/>
     </div>
