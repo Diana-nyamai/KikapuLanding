0dc9ef2ba71d21ea0bbdb0da5b0db5a230a0a84f
Diana-nyamai <dnyamai.dn@gmail.com>
2023-10-26 22:26:45 +0300

    Footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,30 @@ import {BsLinkedin, BsTwitter, BsInstagram} from 'react-icons/bs'
 export const Footer = () => {
   return (
     <div className="footerContainer">
-         <h2>
+      <div className="footerLogo">
+        <h2>
         Kikapu<span style={{color: '#18D26E'}}>.</span>
       </h2>
+      </div>
       
-      <p>copyright 2023</p>
+      <div className="footerDetails">
+        <div className="topInfo">
+          <p className='contact'>info@gmail.com</p>
+          <p className='pipe'></p>
+          <p>+2547 23 456 789</p>
+        </div>
+        
+        <p style={{textAlign: 'center'}} className='copyRemove'> &copy; Copyright 2023 Kikapu. All Right Reserved.</p>
+      </div>
+      
 
       <div className="FooterSocials">
             <BsLinkedin/>
             <BsTwitter/>
             <BsInstagram/>
       </div>
+
+      <p style={{textAlign: 'center'}} className='copyright'> &copy; Copyright 2023 Kikapu. All Right Reserved.</p>
     </div>
   )
 }
